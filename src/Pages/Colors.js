ade160db4ea120e3b9753159c96978056a516f83
Nicolas Djambazian <nicolas@djambazian.fr>
2018-02-04 12:38:30 +0100

    Add foregroundParallaxRatio example

--- src/Pages/Colors.js
+++ src/Pages/Colors.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, View, Dimensions } from 'react-native';
+import { Image, View, Text, StyleSheet } from 'react-native';
 
 import HeaderImageScrollView from 'react-native-image-header-scroll-view';
 
@@ -8,13 +8,19 @@ class ColorsPage extends React.Component {
     return (
       <View style={{ flex: 1 }}>
         <HeaderImageScrollView
-          maxHeight={150}
+          maxHeight={300}
           minHeight={80}
           fadeOutForeground
           headerImage={require('../../assets/cutecat.jpg')}
           overScrollMode="never"
           overlayColor="#4A148C"
           maxOverlayOpacity={0.9}
+          renderForeground={() => (
+            <View style={styles.titleContainer}>
+              <Text style={styles.imageTitle}>Cat</Text>
+            </View>
+          )}
+          foregroundParallaxRatio={3}
         >
           <View style={{ height: 100, backgroundColor: '#4CAF50' }} />
           <View style={{ height: 100, backgroundColor: '#F44336' }} />
@@ -29,5 +35,19 @@ class ColorsPage extends React.Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  imageTitle: {
+    color: 'white',
+    backgroundColor: 'transparent',
+    fontSize: 24,
+  },
+});
 
 export default ColorsPage;
