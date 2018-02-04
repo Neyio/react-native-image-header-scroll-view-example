0c61ecd9a5adb8d6e277224d4cd8a6c1dcfae5b7
Nicolas Djambazian <nicolas@djambazian.fr>
2018-02-04 09:29:37 +0100

    Add the code examples

---
+++ src/App.js
@@ -0,0 +1,47 @@
+// @flow
+import { StackNavigator } from 'react-navigation';
+
+import { Menu, TvShow, BasicUsage, ColorsPage, Avignon, PullToRefresh } from './Pages';
+
+export const App = StackNavigator(
+  {
+    menu: {
+      screen: Menu,
+    },
+    tvShow: {
+      screen: TvShow,
+    },
+    basicUsage: {
+      screen: BasicUsage,
+    },
+    colors: {
+      screen: ColorsPage,
+    },
+    avignon: {
+      screen: Avignon,
+    },
+    pullrefresh: {
+      screen: PullToRefresh,
+    },
+  },
+  {
+    navigationOptions: {
+      headerTintColor: 'white',
+      headerStyle: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        left: 0,
+        elevation: 0,
+        shadowColor: 'transparent',
+        backgroundColor: 'transparent',
+        shadowRadius: 0,
+        shadowOffset: {
+          height: 0,
+        },
+      },
+    },
+  }
+);
+
+export default App;
